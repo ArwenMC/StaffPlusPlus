2094f3e3792357970bc80332933a7873ecf8e532
Christopher Nethercott <ccnethercott@gmail.com>
2019-03-24 14:25:48 +0000

    Added an instance that may be useful

--- src/main/java/com/arwenmc/StaffPlusPlus.java
+++ src/main/java/com/arwenmc/StaffPlusPlus.java
@@ -4,8 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class StaffPlusPlus extends JavaPlugin {
 
+    public StaffPlusPlus instance;
+
     @Override
-    public void onEnable() {}
+    public void onEnable() {
+        this.instance = this;
+    }
 
     @Override
     public void onDisable() {}
