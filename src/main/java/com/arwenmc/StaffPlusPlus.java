a88804727dfe5ca3db52ba473f06776b7d6da015
Christopher Nethercott <ccnethercott@gmail.com>
2019-03-24 14:19:07 +0000

    Create main class

---
+++ src/main/java/com/arwenmc/StaffPlusPlus.java
@@ -0,0 +1,12 @@
+package com.arwenmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StaffPlusPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
